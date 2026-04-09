a5724c2452a5b2bd9610954a66a84d9f2fedde55
mktcode <kontakt@markus-kottlaender.de>
2026-04-09 21:37:01 +0200

    Initialize project with Bun, add essential files and templates for a web application

---
+++ index.ts
@@ -0,0 +1,41 @@
+import Handlebars from "handlebars";
+import data from "./website/data/main.json";
+
+const partials = ["header", "footer", "products"];
+
+for (const name of partials) {
+  const file = Bun.file(`./website/templates/${name}.hbs`);
+  const content = await file.text();
+  Handlebars.registerPartial(name, content);
+}
+
+const websiteTemplate = Bun.file("./website/templates/index.hbs");
+const template = Handlebars.compile(await websiteTemplate.text());
+
+const server = Bun.serve({
+  routes: {
+    "/favicon.ico": Bun.file("./favicon.png"),
+
+    "/": () => {
+      return new Response(template(data), {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
+    },
+
+    "/:page": req => {
+      return new Response(template({ page: req.params.page }), {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
+    },
+  },
+
+  fetch() {
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`Server running at ${server.url}`);
